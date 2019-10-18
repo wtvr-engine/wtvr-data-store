c6336d6b390fc38f0d23ba11da45a32acf5db2b1
John Tiesselune <tiesse@wtvr.rocks>
2019-10-18 16:39:26 +0200

    updated to new file structure

---
+++ src/WTVRDataStore.js
@@ -0,0 +1,43 @@
+export class WTVRDataStore extends HTMLElement {
+    constructor(){
+        super();
+        if(this.hasAttribute("folder")){
+            this.folder = this.getAttribute("folder");
+        }
+        if(this.hasAttribute("backend")){
+            this.backend = this.getAttribute("backend");
+        }else{
+            this.backend = "local";
+        }
+        this.synced = false;
+    }
+
+    async save(key,value){
+        localStorage.setItem(this.getStorageKey(key),JSON.stringify(value));
+        await this.syncUp();
+    }
+    async get(key){
+        await this.syncDown();
+        return JSON.parse(localStorage.getItem(this.getStorageKey(key)));
+    }
+
+    async syncUp(){
+        if(navigator.onLine){
+            return true;
+        }
+        return false;
+    }
+    
+    async syncDown(){
+        if(navigator.onLine && !this.synced){
+            this.synced = true;
+        }
+        return this.synced;
+    }
+
+    getStorageKey(key){
+        if(this.folder){
+            return `${this.folder}-${key}`;
+        }
+    }
+}
--- wtvr-data-store.js
+++ wtvr-data-store.js
@@ -1,43 +1,4 @@
-export default class WTVRDataStore extends HTMLElement {
-    constructor(){
-        super();
-        if(this.hasAttribute("folder")){
-            this.folder = this.getAttribute("folder");
-        }
-        if(this.hasAttribute("backend")){
-            this.backend = this.getAttribute("backend");
-        }else{
-            this.backend = "local";
-        }
-        this.synced = false;
-    }
+import { WTVRDataStore } from './src/WTVRDataStore.js';
+export { WTVRDataStore } from './src/WTVRDataStore.js';
 
-    async save(key,value){
-        localStorage.setItem(this.getStorageKey(key),JSON.stringify(value));
-        await this.syncUp();
-    }
-    async get(key){
-        await this.syncDown();
-        return JSON.parse(localStorage.getItem(this.getStorageKey(key)));
-    }
-
-    async syncUp(){
-        if(navigator.onLine){
-            return true;
-        }
-        return false;
-    }
-    
-    async syncDown(){
-        if(navigator.onLine && !this.synced){
-            this.synced = true;
-        }
-        return this.synced;
-    }
-
-    getStorageKey(key){
-        if(this.folder){
-            return `${this.folder}-${key}`;
-        }
-    }
-}
+customElements.define("wtvr-data-store",WTVRDataStore);
